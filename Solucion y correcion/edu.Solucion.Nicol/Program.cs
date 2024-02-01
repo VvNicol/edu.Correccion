59161cebf4578cf654c4805f8b4eff7de3dd00cc
VvNicol <150023181+VvNicol@users.noreply.github.com>
2024-02-01 10:11:30 +0100

    Add files via upload

---
+++ Solucion y correcion/edu.Solucion.Nicol/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace edu.Adivina
+{
+    internal class Program
+    {
+        private static string jugadorLetra;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adivina la palabra por letras//juego del ahorcado");
+
+            Random random = new Random();
+            string[] palabras = { "misterio","palabra", "gafas" };
+            int mIndex = random.Next(palabras.Length);
+            string palabraSecreta = palabras[mIndex];
+            int numletras = 0;
+
+            int intentos = 6;
+
+            for (int i = 0; i < palabraSecreta.Length; i++)
+            {
+                numletras ++;                         
+            }
+
+            Console.WriteLine("Comienza el juego");
+            Console.WriteLine("La palabra oculta tiene " + numletras + " letras");
+
+            string jugadorPalabra = Console.ReadLine();
+
+
+            while (!jugadorPalabra.Equals(palabraSecreta) && intentos > 0)
+            {
+                Console.WriteLine("Quedan " + intentos + " intento");
+                Console.WriteLine("Inténtalo de nuevo");
+                jugadorPalabra = Console.ReadLine();
+                intentos--;
+            }
+            if (intentos == 0)
+            {
+                Console.WriteLine("Has perdido. La palabra correcta era: " + palabraSecreta);
+            }
+            else
+            {
+                Console.WriteLine("Has ganado");
+            }
+
+
+        }
+    }
+}
---
+++ Solucion y correcion/ejercicioDavidMuñoz/Controladores/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace ejercicioNicole.Controladores
+{
+    /// <summary>
+    /// Clase principal de la aplicación.
+    /// <author>dmp - 310124</author>
+    /// </summary>
+    class program
+    {
+        /// <summary>
+        /// Metodo que contiene toda la aplicación.
+        /// </summary>
+        /// <author>dmp - 310124 </author>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            //Array de tipo string que contiene las palabras.
+            string[] palabrasRandom = { "coche", "ordenador", "java" };
+
+            //Copia de la clase para poder hacer que las palabras sean aleatorias
+            Random rm = new Random();          
+
+            //variable string en la cual la palabra se genera de forma aleatoria
+            string palabraAleatoria = palabrasRandom[rm.Next(0, palabrasRandom.Length)];
+         
+            //Muestra la longitud de la palabra
+            Console.WriteLine("Longitud de la palabra random: " + palabraAleatoria.Length);
+
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("Tienes 5 intentos");
+            Console.WriteLine("-------------------------------------------------------");
+            
+
+            //variable para controlar el bucle for
+            int intentos = 5;
+
+            //bucle for para controlar los intentos
+            for(int i = 0; i < intentos;)
+            {
+                Console.WriteLine("Indique que palabra crees que es la que te ha tocado(todo en minuscula): ");
+                string palabraUsuario = Console.ReadLine();
+                
+                //condicional que comprueba si la palabra introducida es la misma que la palabra
+                // que se ha generado de manera aleatoria.
+                if (palabraUsuario.Equals(palabraAleatoria))
+                {
+                    Console.WriteLine("-------------------------------------------------------");
+                    Console.WriteLine("Has ganado");
+                    Console.WriteLine("-------------------------------------------------------");
+                    return;
+                }
+                else
+                {
+                    i++;
+                    Console.WriteLine("-------------------------------------------------------");
+                    Console.WriteLine("No has acertado");
+                    
+
+                    //muestra los intentos que te quedan
+                    Console.WriteLine("Tienes " + (intentos - i) + " " + "intentos");
+                    Console.WriteLine("-------------------------------------------------------");
+                }
+
+            }
+            //Una vez que termina el bucle te muestra este mensaje
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("Has perdido");
+            Console.WriteLine("La palabra es: " + palabraAleatoria);
+            Console.WriteLine("-------------------------------------------------------");
+        }
+    }
+}
